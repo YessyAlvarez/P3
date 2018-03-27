002ecd849f81b9816ead617f3f9031998ad0c727
jigokux <kaname.x.yuuki@gmail.com>
2018-03-27 20:38:28 -0300

    nuevo 26032018

---
+++ P3-26-3/GestionTramites/InterfazWeb/PerfilAdmin/EditarFuncionario.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using Dominio;
+using InterfazWeb.ServiceReference1;
+
+namespace InterfazWeb.PerfilAdmin
+{
+    public partial class EditarFuncionario : System.Web.UI.Page
+    {
+        ServiceClient miServicio = new ServiceClient();
+        
+    }
+}
---
+++ P3-26-3/GestionTramites/InterfazWeb/PerfilAdmin/EditarGrupo.aspx.designer.cs
@@ -0,0 +1,69 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace InterfazWeb.PerfilAdmin {
+    
+    
+    public partial class EditarGrupo {
+        
+        /// <summary>
+        /// Control PanelValorActual.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel PanelValorActual;
+        
+        /// <summary>
+        /// Control TextBoxValorArancel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxValorArancel;
+        
+        /// <summary>
+        /// Control ButtonGuardarArancel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonGuardarArancel;
+        
+        /// <summary>
+        /// Control PanelMensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel PanelMensaje;
+        
+        /// <summary>
+        /// Control LabelMensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelMensaje;
+        
+        /// <summary>
+        /// Control ButtonVolver.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonVolver;
+    }
+}
