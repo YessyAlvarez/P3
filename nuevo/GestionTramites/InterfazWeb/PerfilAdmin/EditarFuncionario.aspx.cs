d3568c790105548eb203089798d1792f8e9bcc1c
yalvarez <yalvarez@hg.com.uy>
2018-04-19 19:07:07 -0300

    nuevo

---
+++ nuevo/GestionTramites/InterfazWeb/PerfilAdmin/EditarFuncionario.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using Dominio;
+using Dominio.ServiceReference_1;
+
+namespace InterfazWeb.PerfilAdmin
+{
+    public partial class EditarFuncionario : System.Web.UI.Page
+    {
+        ServiceClient miServicio = new ServiceClient();
+        
+    }
+}
---
+++ nuevo/GestionTramites/WCFServices/App_Code/Service.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Dominio;
+using System.Collections.Generic;
+using System.IO;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+public class Service : IService
+{
+    public bool WCFAddGrupo(string nombreGrupo)
+    {
+        bool retorno = true;
+
+        /**
+         * 
+         * lógica
+         * 
+         **/
+
+        return retorno;
+    }
+
+    public bool WCFAddTramite(string titulo, string desc, double costo, int tiempo, List<GrupoTramite> gruposTramite)
+    {
+        Tramite newTramite = new Tramite
+        {
+            Titulo = titulo,
+            Descripcion = desc,
+            Costo = costo,
+            Tiempo = tiempo,
+            Grupos = gruposTramite
+        };
+
+        return newTramite.AgregarTramite();
+    }
+
+    bool IService.WCFGuardarTxt()
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public bool WCFExisteNombreTramite(string nombreTramite)
+    {
+        return Tramite.ExisteNombreTramite(nombreTramite);
+    }
+
+    public List<Grupo> WCFGetGrupo()
+    {
+        return Grupo.listarTodosLosGrupos();
+    }
+
+
+    public Grupo WCFObtenerGrupoPorId(int idGrupo) {
+        return Grupo.ObtenerGrupoPorId(idGrupo);
+    }
+
+}
